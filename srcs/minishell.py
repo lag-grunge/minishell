d1dada12ef49d72e8ea0c347720d42a51aab5001
sdalton <sdalton@student.21-school.ru>
2021-11-12 04:05:28 +0300

    tokens

--- srcs/minishell.py
+++ srcs/minishell.py
@@ -1,54 +1,8 @@
 #! /bin/python
 
 import sys
-
-blanks = [' ', '\t']
-meta = ['|', '&', '(', ')', '<', '>']
-quote = ['\'', '\"']
-
-opers = ['||', '&&', '|']
-brs = ['(', ')']
-redirect = ['>', '<', '>>', '<<']
-
-
-def get_tokens(line):
-    tokens = []
-    j_start = -1
-    tmp_quote = '\0'
-    for (j, i) in enumerate(line):
-        if i == tmp_quote: #если есть цитирование и текущий символ совпадает с начальным символом, то окончить цитирование
-            tmp_quote = '\0'
-        elif tmp_quote != '\0': #в противном случае, если есть цитирование, пропустить текущий символ
-            continue
-        elif i in quote: #если нет цитирования и текущий символ
-            # цитирования - начать цитирование, запомнив начальный символ
-            tmp_quote = i
-            if j > 0 and not line[j - 1] in meta and not line[j - 1] in blanks: #если предыдущий символ не мета и не пробельный не создавать токен
-                continue
-            if j > 0 and line[j - 1] in meta: #если предудыщий символ мета, то записать предыдущий токен
-                tokens.append(line[j_start:j])
-            j_start = j
-        elif i in blanks or i == '\n':
-            if j > 0 and not line[j - 1] in blanks: # если предыдущий не пробельный, то записываем токен
-                tokens.append(line[j_start:j])
-            j_start = -1
-        elif i in meta: #если нет цитирования и текущий символ из мета
-            if j > 0 and j_start != -1 and line[j - 1] in meta and not line[j - 1] in "()": # если уже начат какой-то токен,
-                # a предудыщий относится к мета и не равен скобкам так, то пропустить
-                continue
-            if j > 0 and j_start != -1:
-                tokens.append(line[j_start:j])
-            j_start = j
-        # если нет цитирования, обычный символ
-        else:
-            if j > 0 and line[j - 1] in meta:
-                tokens.append(line[j_start:j])
-            elif j > 0 and not line[j - 1] in blanks:
-                continue
-            j_start = j
-    if tmp_quote != '\0':
-        print("Error: unquoted!")
-    return tokens
+from tokens import get_tokens
+from constants import blanks, meta, quote, opers, brs, redirect
 
 def check_brackets(tokens):
     tmp_br = 0
@@ -159,23 +113,28 @@ if __name__=="__main__":
     # supplied as an argument to the -c invocation option (see Section 6.1 [Invoking Bash],
     # page 87), or from the user’s terminal.
     # with open("tests", "r") as fp:
-    #     while fp.readline():
-    start_line = sys.stdin.readline() # reads input from stdin
+    #      while True:
+    # try:
+    #     start_line = next(fp)
+    # except StopIteration:
+    #     fp.close()
+    #     break;
+            start_line = sys.stdin.readline() # reads input from stdin
 
     # 2. Breaks the input into words and operators, obeying the quoting rules described in
     # Section 3.1.2 [Quoting], page 6. These tokens are separated by metacharacters. Alias
     # expansion is performed by this step (see Section 6.6 [Aliases], page 95).
-    print(start_line)
-    tokens = get_tokens(start_line)
-    print(tokens)
+            print(start_line)
+            tokens = get_tokens(start_line)
+            print(tokens)
 
     # 3. Parses the tokens into simple and compound commands (see Section 3.2 [Shell Com-
     # mands], page 8).
-    if not check_brackets(tokens):
-        command = get_commands(tokens)
-        print_out(command)
-    else:
-        exit(1)
+            if not check_brackets(tokens):
+                command = get_commands(tokens)
+                print_out(command)
+            else:
+                exit(1)
 
     # 4. Performs the various shell expansions (see Section 3.5 [Shell Expansions], page 22),
     # breaking the expanded tokens into lists of filenames (see Section 3.5.8 [Filename Ex-
