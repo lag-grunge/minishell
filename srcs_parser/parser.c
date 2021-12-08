b73ac286fecb1f8f855fa057f412edc547c961c9
sdalton <sdalton@student.21-school.ru>
2021-12-08 20:17:20 +0300

    descent_recursive_parser_accept_except_functions

--- srcs_parser/parser.c
+++ srcs_parser/parser.c
@@ -1,6 +1,81 @@
 #include "minishell.h"
 
-int ft_parser(char *tokens[]) {
+typedef enum {rdr, wrd, op, pp, lb, rb} token;
+
+token	type(char *cur)
+{
+	int ret;
+
+	ret = ft_isoperator(cur);
+	if (ret >= ltlt && ret <= gt)
+		return (rdr);
+	else if (ret == or || ret == and)
+		return (op);
+	else if (ret == pip)
+		return (pp);
+	else if (ret == lbr)
+		return (lb);
+	else if (ret == rbr)
+		return (rb);
+	else
+		return (wrd);
+}
+
+int expect(token t, char ***cur)
+{
+	if (accept(t, cur))
+		return (1);
+	printf("unexpected token\n");
+	return (0);
+}
+
+char **nextsym(char **cur)
+{
+	return (cur + 1);
+}
+
+int accept(token t, char ***cur)
+{
+	if (t == type(**cur))
+	{
+		*cur = nextsym(*cur);
+		return (1);
+	}
+	return (0);
+}
+
+int word(char ***tokens)
+{
+	if (accept(lb, tokens))
+	{
+		printf("unexpected token\n");
+		return (0);
+	}
+	*tokens = nextsym(*tokens);
+	return (1);
+}
+
+int	ft_redir(char ***tokens)
+{
+	return (expect(wrd, tokens));
+}
+
+int	ft_subshell(char ***tokens)
+{
+	int ret;
+
+	ft_parser(*tokens + 1, 1);
+
+}
+
+int oper(char **tokens)
+{
+
+	return (1);
+}
+
+int ft_parser(char *tokens[], int sub)
+{
 	int ret;
 	t_cmd	*cmd;
 	t_opers *opers;
@@ -8,19 +83,14 @@ int ft_parser(char *tokens[]) {
 	opers = ft_oper_new();
 	cmd = ft_cmd_new();
 	opers->oper1 = cmd;
-	while (*tokens)
+	ret = 1;
+	while (*tokens && )
 	{
-		ret = ft_isoperator(*tokens, ft_strlen(*tokens));
-		if (ret == pip)
-			ft_parser_pipe();
-		else if (ret == or  || ret == and)
-			ft_parser_opers();
-		else if (ret == lbr)
-			ft_parser_sbsh();
-		else if (ret >= ltlt || ret <= gtgt)
-			ft_parser_redir();
-		else
-			ft_parser_cmd_args();
+		if (accept(rdr, &tokens))
+			ret = ft_redir(&tokens);
+		else if (accept(wrd, &tokens))
+			ret = ft_word(&tokens);
+		else if (accept(lb, &tokens))
+			ret = ft_subshell(&tokens);
 	}
-
 }
