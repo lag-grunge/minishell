d094e4af1cf2880c288c5df22616371d28d6cc76
Stevenson Dalton <sdalton@il-e5.msk.21-school.ru>
2022-03-11 16:30:33 +0300

    try_openfiles_deleted

--- includes/redirect.h
+++ includes/redirect.h
@@ -5,7 +5,6 @@ int open_for_read(char *filename, int fd_in);
 int open_for_write(char *filename, int fd_out, int append, int err);
 void	ft_redirect(int *pdes, int in_out);
 int ft_openfiles(t_redir *red);
-int try_openfiles(t_stmnt *stmnt);
 int read_here_doc(const char *lim, int fd_in, int *h_doc);
 
 #endif
