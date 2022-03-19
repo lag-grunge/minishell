5d996b68a93ab514f406b06c43a7f0914ddd3a26
Stevenson Dalton <sdalton@oa-a4.msk.21-school.ru>
2022-03-19 03:00:31 +0300

    podayte_suda_ego

--- srcs/expansions/variable.c
+++ srcs/expansions/variable.c
@@ -5,21 +5,25 @@ static int	get_size(char *token)
 	char	*cur;
 	int		exp_num;
 
-	exp_num = 1;
+	exp_num = 0;
 	cur = token;
 	while (*cur)
 	{
-		if (*cur == '\'' || *cur == '\"')
-			cur += quoting(cur);
-		else if (*cur == ' ')
-		{
+		while (*cur == ' ')
+			cur++;
+		if (*cur && *cur != ' ')
 			exp_num++;
-			cur++;
+		while (*cur && *cur != ' ')
+		{
+			if (*cur == '\'' || *cur == '\"')
+				cur += quoting(cur);
+			else
+				cur++;
 		}
-		else
-			cur++;
 	}
-	return (exp_num);
+	if (exp_num)
+		return (exp_num);
+	return (1);
 }
 
 static int	oper_word(t_list *args_list, char **cur_ptr, char *token, int s)
@@ -28,13 +32,10 @@ static int	oper_word(t_list *args_list, char **cur_ptr, char *token, int s)
 	char	*tmp;
 
 	cur = *cur_ptr;
-	if (cur != token)
-	{
-		tmp = ft_substr(token, s, cur - token - s);
-		if (!tmp)
-			exit(malloc_err);
-		write_word(&args_list, tmp);
-	}
+	tmp = ft_substr(token, s, cur - token - s);
+	if (!tmp)
+		exit(malloc_err);
+	write_word(&args_list, tmp);
 	while (*cur == ' ')
 		cur++;
 	return ((int)(cur - token));
@@ -70,13 +71,13 @@ void	variable_expansion(t_list *args_list)
 	token = ft_strdup(args_list->content);
 	exec_expansion(&token);
 	exp_num = get_size(token);
-	if (exp_num == 1)
+	if (exp_num == 1 && ft_strncmp(token, args_list->content, ft_strlen(token) + 1)) 
 	{
 		if (args_list->content)
 			free(args_list->content);
 		args_list->content = ft_strdup(token);
 	}
-	else
+	else if (exp_num > 1)
 	{
 		args_list = ft_lstins_few_empty(args_list, exp_num - 1);
 		expan_list(args_list, token);
