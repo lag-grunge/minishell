7ec176babb20e68c70e807190dece98b4f996df9
Stevenson Dalton <sdalton@oa-c5.msk.21-school.ru>
2022-03-19 13:32:53 +0300

    correct_expan_list_variable_expansion

--- srcs/expansions/variable.c
+++ srcs/expansions/variable.c
@@ -26,23 +26,25 @@ static int	get_size(char *token)
 	return (1);
 }
 
-static int	oper_word(t_list *args_list, char **cur_ptr, char *token, int s)
+static void oper_word(t_list *args_list, char *cur, char *token, int s)
 {
-	char	*cur;
 	char	*tmp;
 
-	cur = *cur_ptr;
-	if (cur != token)
-	{
-		tmp = ft_substr(token, s, cur - token - s);
-		if (!tmp)
-			exit(malloc_err);
-		write_word(&args_list, tmp);
-		while (*cur == ' ')
-			cur++;
-		*cur_ptr = cur;
-	}
-	return ((int)(cur - token));
+    tmp = ft_substr(token, s, cur - token - s);
+    if (!tmp)
+        exit(malloc_err);
+    write_word(&args_list, tmp);
+}
+
+static int  trim_spaces(char **cur_ptr, char *token)
+{
+    char *cur;
+
+    cur = *cur_ptr;
+    while (*cur == ' ')
+        cur++;
+    *cur_ptr = cur;
+    return (cur - token);
 }
 
 static void	expan_list(t_list *args_list, char *token)
@@ -51,9 +53,9 @@ static void	expan_list(t_list *args_list, char *token)
 	int		s;
 
 	cur = token;
-    while (*cur == ' ')
-        cur++;
-    s = cur - token;
+    s = trim_spaces(&cur, token);
+    if (!*cur)
+        oper_word(args_list, cur, token, s);
 	while (*cur)
 	{
 		while (*cur && *cur != ' ')
@@ -65,13 +67,13 @@ static void	expan_list(t_list *args_list, char *token)
 		}
 		if (*cur == ' ')
 		{
-			s = oper_word(args_list, &cur, token, s);
+			oper_word(args_list, cur, token, s);
 			args_list = args_list->next;
+            s = trim_spaces(&cur, token);
 		}
-		
 	}
-	if (cur - token - s > 0)
-		oper_word(args_list, &cur, token, s);
+    if (cur - token - s > 0)
+        oper_word(args_list, cur, token, s);
 }
 
 void	variable_expansion(t_list *args_list)
@@ -82,16 +84,9 @@ void	variable_expansion(t_list *args_list)
 	token = ft_strdup(args_list->content);
 	exec_expansion(&token);
 	exp_num = get_size(token);
-	if (exp_num == 1 && ft_strncmp(token, args_list->content, ft_strlen(token) + 1)) 
-	{
-		if (args_list->content)
-			free(args_list->content);
-		args_list->content = ft_strdup(token);
-	}
-	else if (exp_num > 1)
-	{
-		args_list = ft_lstins_few_empty(args_list, exp_num - 1);
-		expan_list(args_list, token);
-	}
-	free(token);
+	if (exp_num > 1)
+        args_list = ft_lstins_few_empty(args_list, exp_num - 1);
+    if (ft_strncmp(token, args_list->content, ft_strlen(token) + 1))
+        expan_list(args_list, token);
+    free(token);
 }
